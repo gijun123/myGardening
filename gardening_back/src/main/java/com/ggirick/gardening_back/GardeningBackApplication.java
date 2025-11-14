5b178f568168e78fd6e14a933ce4927aec079237
gijun123 <tteenn1@naver.com>
2025-11-14 17:44:12 +0900

    Initial commit

---
+++ gardening_back/src/main/java/com/ggirick/gardening_back/GardeningBackApplication.java
@@ -0,0 +1,13 @@
+package com.ggirick.gardening_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GardeningBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GardeningBackApplication.class, args);
+    }
+
+}
---
+++ gardening_back/src/test/java/com/ggirick/gardening_back/GardeningBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ggirick.gardening_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GardeningBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
