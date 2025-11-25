6fce5638b3e76c7c11c108b58f3c8c970617ed22
Ju-S <wntjd0643@gmail.com>
2025-11-25 17:37:10 +0900

    - 일정 백엔드 연결 완료. - 일정 반복 옵션 구현 예정.

--- gardening_back/src/main/java/com/ggirick/gardening_back/dto/schedule/PatchCalendarDTO.java
+++ gardening_back/src/main/java/com/ggirick/gardening_back/dto/schedule/PatchCalendarDTO.java
@@ -19,6 +19,8 @@ public class PatchCalendarDTO {
     String description;
     @Schema(description = "일정 구분 색상", example = "blue")
     String color;
+    @Schema(description = "일정 부모 ID")
+    Integer recurrenceId;
     @Schema(description = "일정 반복 지정(start_date부터 ~일 마다)", example = "7")
     int recurrence;
     @Schema(description = "일정 반복 종료일", example = "2025-10-14T15:32:40")
